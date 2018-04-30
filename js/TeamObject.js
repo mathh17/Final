4027ad13c120593124475f6a18b1202b7888a943
Unknown <mathh17@student.sdu.dk>
2018-04-30 11:17:27 +0200

    Fixed

--- js/TeamObject.js
+++ js/TeamObject.js
@@ -1,8 +1,9 @@
 
-     function TeamObject (teamName, teamStart, teamEnd, teamTrainer, teamLocation, teamMaxParticipants, teamDescription){
+     function TeamObject (teamName, teamStart, teamEnd, teamDuration, teamTrainer, teamLocation, teamMaxParticipants, teamDescription){
         this.teamName = teamName;
         this.teamStart = teamStart;
         this.teamEnd = teamEnd;
+        this.teamDuration = teamDuration;
         this.teamTrainer = teamTrainer;
         this.teamLocation = teamLocation;
         this.teamMaxParticipants = teamMaxParticipants;
