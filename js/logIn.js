2f377a7e20bcbaecd403728a1e74c314c98c82ca
Unknown <mathh17@student.sdu.dk>
2018-05-03 13:10:35 +0200

    manglede lige noget i js-filen

--- js/logIn.js
+++ js/logIn.js
@@ -1,8 +1,13 @@
-var loggedUser = 
-
+    var emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 function checkForUser(){
     var userNameInput = document.getElementById('username').value;
     var passwordInput = document.getElementById('password').value;
+    
+    
+    var validEmail = emailRegex.test(userNameInput);
+        if(validEmail == false){
+            alert('The email you have entered is invalid')
+        }
     
     for(index in users ){
         var user = userData[index];
