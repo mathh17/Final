334cc6172df8df1852d99fbd7c30bcf5a8b4a9af
DrunkenGenius <troelsdeibjerg@gmail.com>
2018-05-06 23:12:37 +0200

    Reponse on week change, + logging teamId on participate button

--- js/createTable.js
+++ js/createTable.js
@@ -16,26 +16,26 @@ var month;
 var currentWeek = 0;
 
 
- function loadTeamArray() {
-            var lsTeam = localStorage.getItem('newTeam_Data');
-            if (lsTeam != null) {
-                lsTeamsArray = new Array();
-                lsTeamsArray = JSON.parse(lsTeam);
-                $(lsTeamsArray).each(function (index, data) {
-                console.log("index: " + index + ", data: " + data);
-                });
-            }
-        }
+function loadTeamArray() {
+    var lsTeam = localStorage.getItem('newTeam_Data');
+    if (lsTeam != null) {
+        lsTeamsArray = new Array();
+        lsTeamsArray = JSON.parse(lsTeam);
+        $(lsTeamsArray).each(function (index, data) {
+            console.log("index: " + index + ", data: " + data);
+        });
+    }
+}
 
 loadTeamArray();
 
 for (var i = 0; i < lsTeamsArray.length; i++) {
     var weekDay = lsTeamsArray[i].teamActiveDays;
-    for(var j = 0; j < teams.length; j++){
-        for(var e = 0; e < weekDay.length; e++)
-        if(weekDay[e] == j){
-            teams[j].push(lsTeamsArray[i]);
-        }
+    for (var j = 0; j < teams.length; j++) {
+        for (var e = 0; e < weekDay.length; e++)
+            if (weekDay[e] == j) {
+                teams[j].push(lsTeamsArray[i]);
+            }
     }
     console.log(lsTeamsArray);
     console.log(weekDay[0]);
@@ -47,10 +47,10 @@ function buildTable() {
     $('#dynamicTable').append('<table cellspacing="0"></table>');
     $('#dynamicTable table').addClass('mainTable');
     $('.mainTable').append('<thead></thead>' +
-                           '<tbody></tbody>');
-    
+        '<tbody></tbody>');
+
     var buttonHTML = '<br><div align="center" class="cube"><div class="front"> DELTAG </div><div class="bottom"><img class="knapImage" src="pictures/godForm.png"></div><div class="back"> AFMELD </div><div class="top"></div></div><div class="shadow"></div>';
-    
+
     $('#dynamicTable table thead').append('<tr id="tableDates"></tr>');
 
     //Table Header
@@ -67,16 +67,15 @@ function buildTable() {
         var numberOfDaysToAdd = 0;
 
         // 
-      if (currentWeek == 0) {
+        if (currentWeek == 0) {
             if (i < day1) {
                 numberOfDaysToAdd -= day1 - i;
             } else if (i > day1) {
                 numberOfDaysToAdd += i - day1;
             } else {
-                numberOfDaysToAdd = 0 ;
+                numberOfDaysToAdd = 0;
             }
-        }
-        else {
+        } else {
             if (i < day1) {
                 numberOfDaysToAdd -= day1 - i;
                 numberOfDaysToAdd += currentWeek * 7;
@@ -87,7 +86,7 @@ function buildTable() {
                 numberOfDaysToAdd = 0;
                 numberOfDaysToAdd += currentWeek * 7;
             }
-            
+
         }
 
 
@@ -96,20 +95,20 @@ function buildTable() {
 
         day = someDate.getDate();
         month = someDate.getMonth() + 1;
-        
+
         // appends dates to table
         if (month < 10 && day < 10) {
             $("#" + weekday[i]).append(weekday[i] + "<br>0" + day + '-0' + month);
         } else if (month < 10) {
-            $("#" + weekday[i]).append(weekday[i] + "<br>" + day + '-0' + month );
+            $("#" + weekday[i]).append(weekday[i] + "<br>" + day + '-0' + month);
         } else if (day < 10) {
-            $("#" + weekday[i]).append(weekday[i] + "<br>0" + day + '-' + month );
+            $("#" + weekday[i]).append(weekday[i] + "<br>0" + day + '-' + month);
         } else {
-            $("#" + weekday[i]).append(weekday[i] + "<br>" + day + '-' + month );
+            $("#" + weekday[i]).append(weekday[i] + "<br>" + day + '-' + month);
         }
-        
-        
-       for (e = 0; e < teams[i].length; e++) {
+
+
+        for (e = 0; e < teams[i].length; e++) {
             $('.inner' + weekday[i]).append(
                 '<tr><td class="' + teams[i][e].teamName + '-' + day + month + '-' + e + '">' +
                 '</td></tr>'
@@ -122,7 +121,7 @@ function buildTable() {
                 //start and end
                 $('.' + teams[i][e].teamName + '-' + day + month + '-' + e).append('<br>' + teams[i][e].teamStart + '-' + teams[i][e].teamEnd);
                 //team duration
-                $('.' + teams[i][e].teamName + '-' + day + month + '-' + e).append('<br>' + teams[i][e].teamDuration + ' min' );
+                $('.' + teams[i][e].teamName + '-' + day + month + '-' + e).append('<br>' + teams[i][e].teamDuration + ' min');
                 //Location
                 $('.' + teams[i][e].teamName + '-' + day + month + '-' + e).append('<br>' + teams[i][e].teamLocation);
                 //Trainer
@@ -131,18 +130,21 @@ function buildTable() {
                 $('.' + teams[i][e].teamName + '-' + day + month + '-' + e).append('<br> 0/' + teams[i][e].teamMaxParticipants);
                 //participate button
                 $('.' + teams[i][e].teamName + '-' + day + month + '-' + e).append(buttonHTML);
-                    
+
             }
-       }
+        }
     }
-    
-    $(".cube").click(function() {
-                //use a class, since your ID gets mangled
-                if ($(this).hasClass('rotate') == true) {
-                    $(this).removeClass('rotate');
-                } else
-                    $(this).addClass('rotate'); //add the class to the clicked element
-            });
+
+    $(".cube").click(function () {
+        //use a class, since your ID gets mangled
+        if ($(this).hasClass('rotate') == true) {
+            $(this).removeClass('rotate');
+            console.log($(this).closest('td').attr('class'));
+        } else{
+            $(this).addClass('rotate'); //add the class to the clicked element
+            console.log($(this).closest('td').attr('class'));
+            }
+    });
 }
 
 
@@ -170,39 +172,41 @@ $(document).ready(function () {
     }
 
     buildTable();
-    
-    
+
+
 
 
     // Week + Buttons
     $(".buttonMid").text('UGE ' + week.toString());
 
     $(".buttonMidRight").click(function () {
-        if(currentWeek < 2)
-        currentWeek++;
+        if (currentWeek < 2)
+            currentWeek++;
         else
             alert("Det er ikke muligt at booke mere end to uger frem. Vh. God Form :)")
+        
         $(".buttonMid").text('UGE ' + (week + currentWeek).toString());
+        
+        
+        $('#dynamicTable').slideUp(400).slideDown(400);
         buildTable();
     });
     $(".buttonMidLeft").click(function () {
-        $(".buttonMid").text('UGE ' + week.toString());
-         if(currentWeek > 0)
-        currentWeek--;
+        if (currentWeek > 0)
+            currentWeek--;
         else
             alert("Det er ikke muligt at gå tilbage i tiden :)")
-        buildTable();   
+        
+        $(".buttonMid").text('UGE ' + (week + currentWeek).toString());
+        $('#dynamicTable').slideUp(400).slideDown(400);
+        buildTable();
     });
-    
-   // $('.mainTable').stacktable();
-   // $('.mainTable').stackcolumns();
+
+    // $('.mainTable').stacktable();
+    // $('.mainTable').stackcolumns();
 });
 
 
 
 
 // --------------------------------
-
-
-
-
