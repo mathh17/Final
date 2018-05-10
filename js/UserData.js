748dd0579f767726e6bbb208879d9e7d18093095
Unknown <mathh17@student.sdu.dk>
2018-05-10 15:45:26 +0200

    Auto stash before merge of "master" and "origin/master"

--- js/UserData.js
+++ js/UserData.js
@@ -1,4 +1,5 @@
 var userData = 
+<<<<<<< Updated upstream
 [{"id":1,"first_name":"Bruger","last_name":"Bruger","email":"bruger@godform.dk","password":"bruger","phone number":"2902221880","address":"272 Sundown Trail"},
 {"id":2,"first_name":"Guinevere","last_name":"Riddel","email":"griddel1@mayoclinic.com","password":"DLiJu6X","phone number":"5176064943","address":"672 Arapahoe Trail"},
 {"id":3,"first_name":"Mandi","last_name":"Jirusek","email":"mjirusek2@example.com","password":"Zlsm4GFM2v","phone number":"3744195226","address":"82 Browning Circle"},
@@ -101,3 +102,18 @@ var userData =
 {"id":100,"first_name":"Dale","last_name":"Nettleship","email":"dnettleship2r@behance.net","password":"ozwqpMbmyP","phone number":"8699390168","address":"8415 Westridge Court"},
 {"id":101,"first_name":"Admin","last_name":"","email":"admin@godform.dk","password":"admin","phone number":"8699390165","address":"38 Mågevej"},
 {"id":102,"first_name":"Forkert Bruger","last_name":"","email":"awbøaof","password":"aæbwfæuae","phone number":"","address":""}]
+=======
+[{"id":1,"first_name":"Henrie","last_name":"O'Codihie","email":"hocodihie0@freewebs.com","phoneNumber":"2268784292","street_Address":"18702 1st Court","city":"Santo Tomé"},
+{"id":2,"first_name":"Carrol","last_name":"Robins","email":"crobins1@dyndns.org","phoneNumber":"7961382515","street_Address":"86 Anderson Avenue","city":"Naranjito"},
+{"id":3,"first_name":"Amalia","last_name":"MacGow","email":"amacgow2@bloglovin.com","phoneNumber":"4211985438","street_Address":"0 Center Way","city":"Pailin"},
+{"id":4,"first_name":"Christyna","last_name":"Cassedy","email":"ccassedy3@opensource.org","phoneNumber":"9967257253","street_Address":"5 Arapahoe Junction","city":"Shadili"},
+{"id":5,"first_name":"Judy","last_name":"Sorel","email":"jsorel4@tripod.com","phoneNumber":"8108835424","street_Address":"87 Carioca Crossing","city":"København"},
+{"id":6,"first_name":"Ashlen","last_name":"Dumbelton","email":"adumbelton5@wired.com","phoneNumber":"2082835142","street_Address":"7704 Express Drive","city":"Brijest"},
+{"id":7,"first_name":"Elayne","last_name":"Lettuce","email":"elettuce6@delicious.com","phoneNumber":"2741164674","street_Address":"618 Del Sol Street","city":"Sukamahi Satu"},
+{"id":8,"first_name":"Leyla","last_name":"McLanaghan","email":"lmclanaghan7@seesaa.net","phoneNumber":"6371456390","street_Address":"6333 Vermont Park","city":"Rakek"},
+{"id":9,"first_name":"Cordy","last_name":"Maling","email":"cmaling8@sciencedaily.com","phoneNumber":"1339255101","street_Address":"26 Grim Alley","city":"Palmira"},
+{"id":10,"first_name":"Albrecht","last_name":"Balch","email":"abalch9@1688.com","phoneNumber":"2845602376","street_Address":"3330 Golden Leaf Park","city":"Fengshuling"},
+{"id":11,"first_name":"Admin","last_name":"","email":"admin@godform.dk","password":"admin","phone number":"8699390165","address":"38 Mågevej","city":"Ringe"},
+{"id":11,"first_name":"Jakob","last_name":"Most","email":"bruger@godform.dk","password":"bruger","phone number":"7629390161","address":"39 Mågevej","city":"Otterup"},
+{"id":13,"first_name":"Forkert Bruger","last_name":"","email":"awbøaof","password":"aæbwfæuae","phone number":"","address":""}]
+>>>>>>> Stashed changes
