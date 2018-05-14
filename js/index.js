6e6bdc0580a501dc79bf484fbfa6951270c2c870
Unknown <mathh17@student.sdu.dk>
2018-05-14 10:33:18 +0200

    mineBookninger virker på index

--- js/index.js
+++ js/index.js
@@ -22,11 +22,11 @@ for(index in deltag){
     teamNameDeltag.push(deltag[index].split("-"));
 }
 console.log(teamNameDeltag);
-for(var i = 0; i < teamNameDeltag.length; i++){
-    if(teamNameDeltag[i][0] == teamData[i].teamName ){
+for(var j = 0; j < teamNameDeltag.length; j++){
+for(var i = 0; i < teamData.length; i++){
+    if(teamNameDeltag[j][0] == teamData[i].teamName ){
         if(chosenTeam == null){
             chosenTeam = teamData[i];
-            continue;
         }
         else{
             chosenTeam.push(teamData[i]);
@@ -34,13 +34,20 @@ for(var i = 0; i < teamNameDeltag.length; i++){
         }
     }
 }
+}
 console.log(chosenTeam);    
     
 
 
 $(document).ready(function () {
     
-   
+    /*loading participating teams */
+    for(var i = 0; i < chosenTeam.length; i++){
+        $('.deltagerBookninger').append('<div>'+chosenTeam[i].teamName+'<div>');
+        $('.deltagerBookninger').append('<div>' + chosenTeam[i].teamStart + ' - ' + chosenTeam[i].teamEnd +'</div>');
+        $('.deltagerBookninger').append('<div>' + chosenTeam[i].teamLocation + '</div>');
+        $('.deltagerBookninger').append('<div>---------------------------------------------------------------</div>');
+    }
     
     /*The two next put hover effect on all list items in the sidenav*/
     $(".sidenav li").mouseenter(function () {
